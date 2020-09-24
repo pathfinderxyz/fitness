0941726a5d7e576088a35bd765f4dad8a3e19d71
Ramsest01 <56605629+Ramsest01@users.noreply.github.com>
2020-09-24 15:04:22 -0400

    cambios 3
    login

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Dopcoin Login</title>
+    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
